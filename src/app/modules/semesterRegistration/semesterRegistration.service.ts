5d4e48c6f1fb999b637162c70a78217069fc48f5
rupom <furqanrupom978@gmail.com>
2023-12-17 02:47:49 +0600

    make some changes

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -9,16 +9,17 @@ const createSemesterRegistrationIntoDB = async (
   payload: ISemesterRegistration,
 ) => {
   const { academicSemester } = payload;
-  const isSemesterAlreadyRegistered = await SemesterRegistrationModel.findOne({
-    academicSemester,
+
+  /* check any semester registration time in 'UPCOMING' or 'ONGOING'  */
+
+  const isOngoingOrUpcomingSemester = await SemesterRegistrationModel.findOne({
+    $or: [{ status: 'UPCOMING' }, { status: 'ONGOING' }],
   });
 
-  /* checking that semester already registered or not */
-
-  if (isSemesterAlreadyRegistered) {
+  if (isOngoingOrUpcomingSemester) {
     throw new AppError(
-      httpStatus.CONFLICT,
-      'This Semester has been registered !',
+      httpStatus.BAD_REQUEST,
+      `The Semester is already in ${isOngoingOrUpcomingSemester.status}`,
     );
   }
 
@@ -34,19 +35,67 @@ const createSemesterRegistrationIntoDB = async (
     );
   }
 
+  /* checking that semester already registered or not */
+
+  const isSemesterAlreadyRegistered = await SemesterRegistrationModel.findOne({
+    academicSemester,
+  });
+
+  if (isSemesterAlreadyRegistered) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      'This Semester has been registered !',
+    );
+  }
+
   const result = await SemesterRegistrationModel.create(payload);
   return result;
 };
+
+
+/* update semester registrations  */
+
+
+const updateSingleSemesterRegistrationFromDB = async (
+  payload: ISemesterRegistration,
+  id: string,
+) => {
+  /* check that is semester registration is exits or not */
+
+  const isSemesterRegistrationExits =
+    await SemesterRegistrationModel.findById(id);
+
+  if (!isSemesterRegistrationExits) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'Semester registration not found !',
+    );
+  }
+
+  /* check if the semester registrations is ended */
+
+  const requestedRegisteredSemesterStatus = isSemesterRegistrationExits.status;
+
+
+  if (requestedRegisteredSemesterStatus === 'ENDED') {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      'This registration has been closed that is why we cannot update this semester registration',
+    );
+  }
+};
+
+
+
+
+/* get single semester registrations */
 
 const getSingleSemesterRegistrationFromDB = async (id: string) => {
   const result = await SemesterRegistrationModel.findById(id);
   return result;
 };
 
-const updateSingleSemesterRegistrationFromDB = async (
-  payload: ISemesterRegistration,
-  id: string,
-) => {};
+
 
 const getAllSemesterRegistrationFromDB = async (
   query: Record<string, unknown>,
